73fb72fba9aa4ff4f20eed18deafc081da2663be
Suniaster <thibousbr@gmail.com>
2021-08-20 12:28:54 -0300

    Remove * wildcards

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,21 @@ use ndarray::array;
 
 // use lib::neural_net::perceptron::Neuron;
 mod lib;
-use lib::layer::*;
-use lib::neural_net::*;
+// use lib::layer::*;
+// use lib::neural_net::*;
 
 fn main() {
     let input1 = array![1.0, 1.0, 1.0];
 
-    let nn = NeuralNet::from_format(&[3, 2, 1]);
+    let nn = lib::neural_net::NeuralNet::from_format(&[3, 2, 1]);
     println!("{:?}", nn.foward(&input1));
 
-    let nn2 = NeuralNet::create(
+    let nn2 = lib::neural_net::NeuralNet::create(
         vec![
-            Layer::new_dense(3, 2),
-            Layer::ReLu,
-            Layer::new_dense(2, 1),
-            Layer::ReLu,
+            lib::layer::Layer::new_dense(3, 2),
+            lib::layer::Layer::ReLu,
+            lib::layer::Layer::new_dense(2, 1),
+            lib::layer::Layer::ReLu,
         ]
     );
 
